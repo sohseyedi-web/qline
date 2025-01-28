47486751f01f7689db4b80463f10ba69e442fc73
sohseyedi <sohseyedi@gmail.com>
2025-01-28 19:20:09 +0330

    feat: add modal for user login

--- src/app/(main)/_/components/HeaderLayout.tsx
+++ src/app/(main)/_/components/HeaderLayout.tsx
@@ -1,23 +1,33 @@
+"use client";
+import ModalWrapper from "@/components/ModalWrapper";
 import ToggleSwitch from "@/ui/ToggleSwitch";
-import Link from "next/link";
+import { useState } from "react";
 import { RiUserLine } from "react-icons/ri";
 
-
 const HeaderLayout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="py-4 px-5 flex items-center justify-between border-b border-[#e6e7ee]">
+    <header className="py-3 md:px-7 px-5 flex items-center justify-between border-b border-[#e6e7ee]">
       <div className="flex items-center gap-x-3">
         <h2 className="text-3xl font-bold text-[#292a35]">Qلاین</h2>
       </div>
       <div className="flex items-center gap-x-2">
         <ToggleSwitch />
-        <Link
-          href={"/join"}
+        <button
+          onClick={() => setIsOpen(true)}
           className="md:w-[150px] w-[42px] h-[42px] rounded-xl border border-[#e6e7ee] flex items-center justify-center hover:bg-gray-50 transition-all duration-300 text-[#292a33] font-medium"
         >
           <span className="md:block hidden">ورود به کیولاین</span>
-          <RiUserLine size={25} className="md:hidden block"/>
-        </Link>
+          <RiUserLine size={25} className="md:hidden block" />
+        </button>
+        <ModalWrapper
+          isOpen={isOpen}
+          onClose={() => setIsOpen(!isOpen)}
+          title="ورود کاربران"
+        >
+          اثمم
+        </ModalWrapper>
       </div>
     </header>
   );
