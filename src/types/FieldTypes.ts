c7cdaad41d3e4d8f49fd813241f6de9b4688d7ea
sohseyedi <sohseyedi@gmail.com>
2025-01-28 21:48:23 +0330

    feat: add TypeScript types for form field validation

---
+++ src/types/FieldTypes.ts
@@ -0,0 +1,20 @@
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+export type ValidationSchemaTypes = {
+  required?: string;
+  minLength?: {
+    value: number;
+    message: string;
+  };
+};
+
+export type TextFieldTypes = {
+  label: string;
+  name: string;
+  placeholder?: string;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+  validationSchema?: ValidationSchemaTypes;
+  value?: string;
+  onChange?: React.ChangeEventHandler;
+};
