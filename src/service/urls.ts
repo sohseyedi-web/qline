92cd4470aa2dd8ddbb41a2ba0c18b39899e6124d
sohseyedi <sohseyedi@gmail.com>
2025-01-28 21:48:09 +0330

    feat: add API endpoint URLs for user authentication and profile management

---
+++ src/service/urls.ts
@@ -0,0 +1,6 @@
+export const GET_OTP = `/user/get-otp`;
+export const CHECK_OTP = `/user/check-otp`;
+export const COMPLETE_PROFILE = `/user/complete-profile`;
+
+export const GET_USER_DETAIL = `/user/profile`;
+export const LOG_OUT = `/user/logout`;
