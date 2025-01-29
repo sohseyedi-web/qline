f993462b6aa58a45bb2e7075df68a9ec99a42bd0
sohseyedi <sohseyedi@gmail.com>
2025-01-29 17:58:58 +0330

    feat: add Back component responsive

---
+++ src/ui/Back.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { useResponsiveStore } from "@/store/useResponsive";
+
+const Back = () => {
+  const { active, setActive } = useResponsiveStore();
+
+  return (
+    active && (
+      <div
+        className="lg:hidden fixed block w-full h-full top-0 left-0 bg-[rgba(0,0,0,.5)] z-30 transition-all duration-300"
+        onClick={() => setActive(!active)}
+      ></div>
+    )
+  );
+};
+
+export default Back;
