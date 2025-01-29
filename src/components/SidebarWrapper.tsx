997fced4499441fa0d3c55f97b69fe64d30dc082
sohseyedi <sohseyedi@gmail.com>
2025-01-29 17:59:25 +0330

    feat: create SidebarWrapper component for responsive sidebar navigation

---
+++ src/components/SidebarWrapper.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useResponsiveStore } from "@/store/useResponsive";
+import Back from "@/ui/Back";
+
+const SidebarWrapper = ({ children }: { children: React.ReactNode }) => {
+  const { active } = useResponsiveStore();
+
+  return (
+    <>
+      <Back />
+      <aside
+        className={`${
+          active ? " right-0 top-0" : "-right-80 top-0"
+        } fixed z-40 w-[280px] lg:relative bg-zinc-50 h-screen lg:h-auto border-l-2 border-[#e6e7ee] space-y-3 transition-all duration-300`}
+      >
+        {children}
+      </aside>
+    </>
+  );
+};
+
+export default SidebarWrapper;
