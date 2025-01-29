783a577d35d5909d2c2653d908659719b001d4da
sohseyedi <sohseyedi@gmail.com>
2025-01-29 09:59:20 +0330

    feat: create SendOtp component for user authentication

---
+++ src/app/(main)/_/components/auth/SendOTP.tsx
@@ -0,0 +1,32 @@
+import { SendOtpTypes } from "@/types";
+import Button from "@/ui/Button";
+import TextField from "@/ui/TextField";
+
+const SendOtp = ({ onSubmit, loading, register, errors }: SendOtpTypes) => {
+  return (
+    <form className="space-y-5 mt-3 w-full" onSubmit={onSubmit}>
+      <p className="my-3">برای ورود یا ثبت‌نام، اطلاعات کاربری خود را وارد کنید:</p>
+      <TextField
+        label={""}
+        placeholder="شماره موبایل"
+        name={"phoneNumber"}
+        register={register}
+        errors={errors}
+        validationSchema={{
+          required: "شماره موبایل ضرروی است",
+          minLength: {
+            value: 11,
+            message: "شماره موبایل اشتباه است",
+          },
+        }}
+      />
+      <Button
+        className={"text-white hover:bg-cyan-600 bg-cyan-800"}
+        title="ورود / ثبت نام"
+        loading={loading}
+      />
+    </form>
+  );
+};
+
+export default SendOtp;
