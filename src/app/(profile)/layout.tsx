14d54f706361edd3a9a71be0ec84b1b76e9a1b14
sohseyedi <sohseyedi@gmail.com>
2025-01-29 10:33:35 +0330

    feat: create RootLayout component for user profile

---
+++ src/app/(profile)/layout.tsx
@@ -0,0 +1,29 @@
+import { Toaster } from "react-hot-toast";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import vazirFont from "@/constants/localFonts";
+import "@/styles/globals.css";
+
+export const metadata = {
+  title: "کیولاین / پنل کاربری ",
+  description: "کیولاین برای ساخت پرسشنامه فرم و نظرسنجی آنلاین",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body
+        suppressHydrationWarning={true}
+        className={`${vazirFont.variable} font-sans`}
+      >
+        <ReactQueryProvider>
+          <Toaster />
+          {children}
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
+}
