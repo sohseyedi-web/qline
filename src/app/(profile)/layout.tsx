745527e206acd2440392f7b0e87ecd4debfbc163
sohseyedi <sohseyedi@gmail.com>
2025-01-29 17:59:47 +0330

    chore: integrate Sidebar and HeaderProfile components into RootLayout

--- src/app/(profile)/layout.tsx
+++ src/app/(profile)/layout.tsx
@@ -2,6 +2,8 @@ import { Toaster } from "react-hot-toast";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import vazirFont from "@/constants/localFonts";
 import "@/styles/globals.css";
+import Sidebar from "./_/components/Sidebar";
+import HeaderProfile from "./_/components/HeaderProfile";
 
 export const metadata = {
   title: "کیولاین / پنل کاربری ",
@@ -20,8 +22,14 @@ export default function RootLayout({
         className={`${vazirFont.variable} font-sans`}
       >
         <ReactQueryProvider>
+          <main className="flex h-[100vh]">
+            <Sidebar />
+            <section className="flex-1">
+              <HeaderProfile/>
+              {children}
+            </section>
+          </main>
           <Toaster />
-          {children}
         </ReactQueryProvider>
       </body>
     </html>
