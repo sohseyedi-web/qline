8c74abe33a614c8f2ce42456d026d67aab576fa9
sohseyedi <sohseyedi@gmail.com>
2025-01-29 10:30:40 +0330

    choren: replace button with UserAuthBtn

--- src/app/(main)/_/components/HeaderLayout.tsx
+++ src/app/(main)/_/components/HeaderLayout.tsx
@@ -2,8 +2,8 @@
 import ModalWrapper from "@/components/ModalWrapper";
 import ToggleSwitch from "@/ui/ToggleSwitch";
 import { useState } from "react";
-import { RiUserLine } from "react-icons/ri";
 import AuthContainer from "./auth/AuthContainer";
+import UserAuthBtn from "./UserAuthBtn";
 
 const HeaderLayout = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,13 +15,7 @@ const HeaderLayout = () => {
       </div>
       <div className="flex items-center gap-x-2">
         <ToggleSwitch />
-        <button
-          onClick={() => setIsOpen(true)}
-          className="md:w-[150px] w-[42px] h-[42px] rounded-xl border border-[#e6e7ee] flex items-center justify-center hover:bg-gray-50 transition-all duration-300 text-[#292a33] font-medium"
-        >
-          <span className="md:block hidden">ورود به کیولاین</span>
-          <RiUserLine size={25} className="md:hidden block" />
-        </button>
+        <UserAuthBtn onOpen={() => setIsOpen(true)} />
         <ModalWrapper
           className="lg:w-[28%] md:w-[50%]"
           isOpen={isOpen}
