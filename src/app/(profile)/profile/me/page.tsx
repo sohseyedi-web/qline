89ea1c4da8ce882f0b5de76f4ecde118827461a3
sohseyedi <sohseyedi@gmail.com>
2025-01-30 13:57:43 +0330

    chore: add UserDataForm component into profile page

--- src/app/(profile)/profile/me/page.tsx
+++ src/app/(profile)/profile/me/page.tsx
@@ -1,5 +1,13 @@
+import UserDataForm from "../../_/components/UserDataForm";
+
 export default function Me() {
-    return(
-        <div>me</div>
-    )
+  return (
+    <section className="p-3 mt-12">
+      <header className="flex items-center justify-between">
+        <h5 className="text-xl font-semibold ">اطلاعات کاربری</h5>
+      </header>
+      <hr className="mb-4 mt-1 border-[#e6e7ee]" />
+      <UserDataForm />
+    </section>
+  );
 }
