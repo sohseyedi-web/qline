299b5dbaa84848aac986796aa516dd7fc55e9d17
sohseyedi <sohseyedi@gmail.com>
2025-01-30 13:57:10 +0330

    chore: add UPDATE_PROFILE URL for user profile updates

--- src/service/urls.ts
+++ src/service/urls.ts
@@ -3,4 +3,5 @@ export const CHECK_OTP = `/user/check-otp`;
 export const COMPLETE_PROFILE = `/user/complete-profile`;
 
 export const GET_USER_DETAIL = `/user/profile`;
+export const UPDATE_PROFILE = `/user/update`;
 export const LOG_OUT = `/user/logout`;
