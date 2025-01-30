ff353766e3ac21a5101965b72e7198048d3038f9
sohseyedi <sohseyedi@gmail.com>
2025-01-30 11:55:19 +0330

    chore: add CreateBox component into ProfilePage

--- src/app/(profile)/profile/page.tsx
+++ src/app/(profile)/profile/page.tsx
@@ -1,5 +1,10 @@
+import CreateBox from "../_/components/create/CreateBox";
+
 export default function ProfilePage() {
-    return(
-        <section className="p-3">soheil</section>
-    )
+    return (
+      <section className="p-3">
+        <CreateBox/>
+        <hr className="my-4 border-[#e6e7ee]"/>
+      </section>
+    );
 }
