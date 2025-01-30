499cb96d582bf3927b45359a0d53392e24eba761
sohseyedi <sohseyedi@gmail.com>
2025-01-30 11:54:13 +0330

    style: adjust z-index of SidebarWrapper

--- src/components/SidebarWrapper.tsx
+++ src/components/SidebarWrapper.tsx
@@ -11,7 +11,7 @@ const SidebarWrapper = ({ children }: { children: React.ReactNode }) => {
       <aside
         className={`${
           active ? " right-0 top-0" : "-right-80 top-0"
-        } fixed z-40 w-[280px] lg:relative bg-zinc-50 h-screen lg:h-auto border-l-2 border-[#e6e7ee] space-y-3 transition-all duration-300`}
+        } fixed z-10 w-[280px] lg:relative bg-zinc-50 h-screen lg:h-auto border-l-2 border-[#e6e7ee] space-y-3 transition-all duration-300`}
       >
         {children}
       </aside>
