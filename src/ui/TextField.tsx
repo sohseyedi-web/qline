a5b99da5924e3107e6486421e560b6cd07ef8c7e
sohseyedi <sohseyedi@gmail.com>
2025-01-30 13:56:37 +0330

    chore: update TextField component to support generic types

--- src/ui/TextField.tsx
+++ src/ui/TextField.tsx
@@ -1,7 +1,7 @@
 import { TextFieldTypes } from "@/types";
-import { get } from "react-hook-form";
+import { get, Path } from "react-hook-form";
 
-function TextField({
+function TextField<T extends Record<string, any>>({
   label,
   name,
   placeholder = "",
@@ -10,26 +10,26 @@ function TextField({
   validationSchema,
   value,
   onChange,
-}: TextFieldTypes) {
+}: TextFieldTypes<T>) {
   return (
     <div className="w-full relative">
-      <label htmlFor={name} className="block mb-1 text-zinc-700">
+      <label htmlFor={name as string} className="block mb-1 text-zinc-700">
         {label}
       </label>
       <input
         autoComplete="off"
         className="placeholder:text-zinc-600 mb-2 input input-bordered text-zinc-800 w-full focus:bg-gray-100 bg-gray-50 h-[45px] text-center transition-all duration-300 rounded-xl outline-none"
         type="text"
-        id={name}
+        id={name as string}
         value={value}
         defaultValue={value}
         placeholder={placeholder}
-        {...register(name, validationSchema)}
+        {...register(name as Path<T>, validationSchema)}
         onChange={onChange}
       />
-      {get(errors, `${name}.message`, null) && (
+      {get(errors, `${name as string}.message`, null) && (
         <span className="text-red-500 font-semibold">
-          {get(errors, `${name}.message`, null)}
+          {get(errors, `${name as string}.message`, null)}
         </span>
       )}
     </div>
