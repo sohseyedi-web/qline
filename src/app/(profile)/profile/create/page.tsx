084fcaad3f1a64006ffcee5ed329941f545c60d4
sohseyedi <sohseyedi@gmail.com>
2025-01-30 18:13:35 +0330

    feat: implement dynamic form rendering based on selection in CreatePage

--- src/app/(profile)/profile/create/page.tsx
+++ src/app/(profile)/profile/create/page.tsx
@@ -3,6 +3,9 @@ import useQlineStore from "@/store/useQlineStore";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Link from "next/link";
+import SurveyForm from "../../_/components/create/OnlineForm";
+import QuestionnaireForm from "../../_/components/create/QuestionnaireForm";
+import OnlineForm from "../../_/components/create/OnlineForm";
 
 export default function CreatePage() {
   const selection = useQlineStore((state) => state.selection);
@@ -12,7 +15,7 @@ export default function CreatePage() {
     if (!selection) {
       router.push("/profile");
     }
-  },[selection,router]);
+  }, [selection, router]);
 
   return (
     <section className="p-3">
@@ -20,14 +23,18 @@ export default function CreatePage() {
         <h6 className="text-2xl font-semibold text-[#292a35]">
           ایجاد {selection?.name}
         </h6>
-        <Link
-          href={"/guide"}
-          className="text-cyan-700 flex items-center"
-        >
-         راهنمای کیولاین
+        <Link href={"/guide"} className="text-cyan-700 flex items-center">
+          راهنمای کیولاین
         </Link>
       </header>
       <hr className="mb-4 mt-1 border-[#e6e7ee]" />
+      {selection?.id == 1 ? (
+        <QuestionnaireForm />
+      ) : selection?.id == 2 ? (
+        <SurveyForm />
+      ) : (
+        <OnlineForm />
+      )}
     </section>
   );
 }
